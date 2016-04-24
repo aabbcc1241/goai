6609a9eada9f4d9308ebf80faf03c8a732006ef8
Beeno Tung <aabbcc1241@yahoo.com.hk>
2016-04-25 00:47:24 +0800

    updated ga init approach

--- ga/ga.go
+++ ga/ga.go
@@ -16,26 +16,21 @@ type Gene_s struct {
 	Fitness Float64PreCompute
 }
 type GA_s struct {
-	P_Crossover float64
-	P_Mutation  float64
 	Population  []Gene_s
+	P_CrossOver float64
+	P_Mutation  float64
 }
 
-func Init_Basic(n_pop int, gen_length int, p_crossover float64, p_mutation float64) GA_s {
-	population := make([]Gene_s, n_pop)
-	for i := 0; i < n_pop; i++ {
+func (p*GA_s)Init(n_pop int, gen_length int) {
+	p.Population = make([]Gene_s, n_pop)
+	for i_pop := 0; i_pop < n_pop; i_pop++ {
 		code := make([]byte, gen_length)
 		rand.Read(code)
 		for i := 0; i < gen_length; i++ {
 			code[i] = code[i] % 2
 		}
-		population[i] = Gene_s{
+		p.Population[i_pop] = Gene_s{
 			Code:code,
 		}
-	}
-	return GA_s{
-		P_Crossover:p_crossover,
-		P_Mutation:p_mutation,
-		Population:population,
 	}
 }
--- test.go
+++ test.go
@@ -2,29 +2,18 @@ package main
 
 import (
 	"fmt"
+
 	"github.com/aabbcc1241/goai/ga"
+	"time"
 )
 
 func main() {
 	fmt.Println("start")
+	fmt.Println(time.Now())
 
-	ga_profile := ga.Init_Basic(100, 1000, 0.5, 0.01)
-	c1 := 0
-	c2 := 0
-	for _, v := range (ga_profile.Population) {
-		for _, v := range (v.Code) {
-			if v == 0 {
-				c1++
-			} else if v == 1 {
-				c2++
-			} else {
-				fmt.Println("wrong value", v, v % 2)
-			}
-		}
-	}
-	fmt.Println("population", ga_profile.Population)
-	fmt.Println("0 count:", c1, "1 count:", c2)
-	fmt.Println("ratio", float64(c1) / float64(c1 + c2))
+	ga_s:=ga.GA_s{}
+	ga_s.Init(100,1000)
 
 	fmt.Println("end")
+	fmt.Println(time.Now())
 }
