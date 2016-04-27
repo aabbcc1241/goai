3a25f7bdbca9f3a7903ecff18885289d64fdc6d3
Beeno Tung <aabbcc1241@yahoo.com.hk>
2016-04-28 00:56:02 +0800

    implemented crossover, mutation, runUntil

--- ga/ga.go
+++ ga/ga.go
@@ -1,8 +1,8 @@
 package ga
 
 import (
-	"math/rand"
 	"github.com/aabbcc1241/goutils/log"
+	"math/rand"
 	"sort"
 )
 
@@ -14,34 +14,38 @@ type Float64PreCompute struct {
 	IsValid bool
 }
 
-func (p *Float64PreCompute)Set(v float64) {
+func (p *Float64PreCompute) Set(v float64) {
 	p.Value = v
 	p.IsValid = true
 }
 
+type code_t []byte
 type Gene_s struct {
-	Code    []byte
+	Code    code_t
 	Fitness Float64PreCompute
 }
 type population []Gene_s
 type GA_s struct {
 	Population  population
 	P_CrossOver float64
-	P_Mutation  float64
-	Fitness_i   Fitness_i
+	/* probability of mutation */
+	P_Mutation float64
+	/* amount (percentage) of mutation within that gen */
+	A_Mutation float64
+	Fitness_i  Fitness_i
 }
 
-func (p population)Len() int {
+func (p population) Len() int {
 	return len(p)
 }
-func (p population)Swap(i, j int) {
+func (p population) Swap(i, j int) {
 	p[i], p[j] = p[j], p[i]
 }
-func (p population)Less(i, j int) bool {
+func (p population) Less(i, j int) bool {
 	return p[i].Fitness.Value < p[j].Fitness.Value
 }
 
-func (p *GA_s)Init(n_pop int, gen_length int) {
+func (p *GA_s) Init(n_pop int, gen_length int) {
 	p.Population = make([]Gene_s, n_pop)
 	for i_pop := 0; i_pop < n_pop; i_pop++ {
 		code := make([]byte, gen_length)
@@ -50,37 +54,94 @@ func (p *GA_s)Init(n_pop int, gen_length int) {
 			code[i] = code[i] % 2
 		}
 		p.Population[i_pop] = Gene_s{
-			Code:code,
+			Code: code,
 		}
 	}
 }
-func (p *GA_s)Run() {
+
+/* equally pick from parent */
+func (child *code_t) crossover(parent1, parent2 *code_t) {
+	for i := range *child {
+		if rand.Intn(2) == 0 {
+			(*child)[i] = (*parent1)[i]
+		} else {
+			(*child)[i] = (*parent2)[i]
+		}
+	}
+}
+
+/* equal-likely single bit flip */
+func (p *code_t) mutation(a_mutation float64) {
+	for i := range *p {
+		if rand.Float64() < a_mutation {
+			(*p)[i] = 1 - (*p)[i]
+		}
+	}
+}
+func measure_and_sort(p *GA_s) (bestFitness float64) {
+	for i := range p.Population {
+		if !p.Population[i].Fitness.IsValid {
+			p.Population[i].Fitness.Set(p.Fitness_i.Apply(p.Population[i]))
+		}
+	}
+	sort.Sort(p.Population)
+	return p.Population[len(p.Population)-1].Fitness.Value
+}
+
+/* REMARK : require pre-sorted population */
+func crossover(p *GA_s) {
 	n_pop := len(p.Population)
+	n_crossover := (int)(p.P_CrossOver * float64(n_pop))
+	/* replace first p_mutation percent for bad gens */
+	for i_pop := 0; i_pop < n_crossover; i_pop++ {
+		/* select from last (1-p_mutation) parent pool */
+		a := n_crossover + rand.Intn(n_pop-n_crossover)
+		b := n_crossover + rand.Intn(n_pop-n_crossover)
+		p.Population[i_pop].Code.crossover(&p.Population[a].Code, &p.Population[b].Code)
+		p.Population[i_pop].Fitness.IsValid = false
+	}
+}
+func mutation(p *GA_s) {
+	for i := range p.Population {
+		if rand.Float64() < p.P_Mutation {
+			p.Population[i].Code.mutation(p.A_Mutation)
+		}
+	}
+}
+func (p *GA_s) Run() {
 	//gen_length := len(p.Population[0].Code)
 	/* 1. get fitness */
-	for i := range (p.Population) {
-		p.Population[i].Fitness.Set(p.Fitness_i.Apply(p.Population[i]))
-	}
-	/* 2. crossover TODO */
-	sort.Sort(p.Population)
-	log.Info.Println("best:", p.Population[n_pop - 1].Fitness.Value)
-	new_pop := make([]Gene_s, n_pop)
-	for i_pop := 0; i_pop < n_pop; i_pop++ {
-		a := i_pop - i_pop % 2
-		//b:=a+1
-		code := p.Population[a].Code
-		new_pop[i_pop] = Gene_s{
-			Code:code,
-		}
-	}
-	p.Population = new_pop
+	bestFitness := measure_and_sort(p)
+	/* 2. crossover */
+	log.Info.Println("bestFitness:", bestFitness)
+	crossover(p)
 	/* 3. mutation TODO */
+	mutation(p)
 }
-func (p *GA_s)RunN(n int) {
+func (p *GA_s) RunN(n int) {
 	for i := 0; i < n; i++ {
 		log.Info.Printf("%v/%v", i, n)
 		p.Run()
 	}
 	log.Info.Printf("%v/%v", n, n)
 }
+func (p *GA_s) RunUntil(targetBestFitness float64, stepUpperLimit int) (stepCount int, excessUpperLimit bool) {
+	currentBestFitness := 0.0
+	i := 0
+	for {
+		i++
+		currentBestFitness = measure_and_sort(p)
+		//log.Info.Println("step:", i, "bestFitness:", currentBestFitness)
+		if currentBestFitness >= targetBestFitness {
+			break
+		}
+		if i > stepUpperLimit {
+			return i, true
+		}
+		crossover(p)
+		mutation(p)
+	}
+	return i, false
+}
+
 //func (p *GA_s)RunWhile
--- test.go
+++ test.go
@@ -4,21 +4,42 @@ import (
 	"fmt"
 
 	"github.com/aabbcc1241/goai/ga"
-	"time"
 	"github.com/aabbcc1241/goutils/log"
+	"time"
 )
 
+/* demo application of the ga
+ *   maximizing number of 1 in gen code
+ *     a_mutation     | stepCount for all code to be 1
+ *       0.0001       | 7507
+ *       0.0005833333 | 3865
+ *       0.00065      | 3499
+ *       0.000825     | 1323
+ *       0.00086875   | 1199
+ *       0.0009       | 10001 (excess limit)
+ *       0.00091      | 830
+ *       0.000911     | 642
+ *       0.000912     | 642
+ *       0.0009125    | 642
+ *       0.0009128    | 642
+ *       0.000913     | 926
+ *       0.000914     | 1281
+ *       0.000915     | 1351
+ *       0.00092      | 10001 (excess limit)
+ *       0.000934375  | 1821
+ *       0.001        | 2153
+ * the parameter is for user application initial guess reference
+ */
 func init() {
 	log.Init(true, true, true, log.ShortCommFlag)
 }
 
 type Fitness_i struct {
-
 }
 
-func (Fitness_i)Apply(gen ga.Gene_s) float64 {
+func (Fitness_i) Apply(gen ga.Gene_s) float64 {
 	i := float64(0)
-	for _, v := range (gen.Code) {
+	for _, v := range gen.Code {
 		i += float64(v)
 	}
 	//log.Debug.Println("fitness:",i)
@@ -29,10 +50,15 @@ func main() {
 	fmt.Println(time.Now())
 
 	ga_s := ga.GA_s{
-		Fitness_i:Fitness_i{},
+		P_CrossOver: 0.8,
+		P_Mutation:  0.2,
+		A_Mutation:  0.000912,
+		Fitness_i:   Fitness_i{},
 	}
 	ga_s.Init(100, 1000)
-	ga_s.RunN(100)
+	//ga_s.RunN(10000)
+	stepCount, excessLimit := ga_s.RunUntil(1000, 10000)
+	log.Info.Println("stepCount", stepCount, "earlyTerm", excessLimit)
 
 	fmt.Println("end")
 	fmt.Println(time.Now())
